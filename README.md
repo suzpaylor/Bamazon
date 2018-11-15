be219af36d6ecd5c298c3ae41f0fa3fff4d3b661
The Lizard <suzpaylor@gmail.com>
2018-11-14 20:21:57 -0700

    created schema, gitignore, readME

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Bamazon
-Node.js &amp; MySQL app - Trilogy Coding Bootcamp
+# bamazon
+
+# Node.js & MySQL
+
+Overview
+
+In this activity, we created an Amazon-like storefront with the MySQL skills from this unit. The app takes in orders from customers and deplete stock from the store's inventory.
+
+**HERE ARE SCREENSHOTS DEMONSTRATING THE PROCESS**
+
+Scott Paylor aka Santa if you searched through my code like I told you earlier I wouldn't mind any of these gifts this year.
---
+++ bamazon_schema.sql
@@ -0,0 +1,29 @@
+CREATE database bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  item_id INT(10) AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT (100) NOT NULL,
+  PRIMARY KEY (item_id)
+);
+
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+("Aqua Lily Pad", "Outdoors", 799.00, 40),
+("Heated Gloves", "Outdoors", 139.00, 10),
+("Electronic Scooter", "Electronics", 599.00, 80),
+("Electric Blanket", "Home", 49.00, 15),
+("Nordica 2019 Santa Ana 110 Women's Skis", "Downhill Skiing", 925.00, 10),
+("Women's Sunrise Shredder Rainbow Ski Suit", "Downhill Skiing", 170.00, 2),
+("Women's Base Layer Ninja Suit", "Outdoors", 109.00, 20),
+("Hotronic FootWarmer", "Outdoors", 184.00, 40),
+("Cottoncandy Machine", "Kitchen", 655.00, 20),
+("Oakley Snowgoggles", "Downhill Skiing", 159.60, 10);
+
+SELECT * FROM products;
